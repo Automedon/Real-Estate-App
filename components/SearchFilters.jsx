c952dcbde83a90fbacfa12e2897915a905c8c907
Automedon <42299107+Automedon@users.noreply.github.com>
2021-12-16 12:36:19 +0700

    Filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,20 +1,43 @@
-import { useState, useEffect } from "react";
-import {
-  Flex,
-  Select,
-  Box,
-  Text,
-  Input,
-  Spinner,
-  Icon,
-  Button,
-} from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Select, Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { MdCancel } from "react-icons/md";
-import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>Search</div>;
+  const [filters] = useState(filterData);
+  const router = useRouter();
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    getFilterValues(filterValues).forEach((item) => {
+      query[item.name] = item.value;
+    });
+
+    router.push({ pathname: path, query });
+  };
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flex="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
